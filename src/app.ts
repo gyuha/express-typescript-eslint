41c7dd5a2928f5b021513699fd745281946a336e
gyuha <nicegyuha@gmail.com>
2020-06-11 01:16:04 +0900

    update

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,9 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req: express.Request, res: express.Response) => {
-  res.send('Hello World!');
+  res.send('Hello World!')
 });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
-});
+})
